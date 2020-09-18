487f3a8f9024d8560d1d950a1a17f737a114c7b2
Milton <A01634505@itesm.mx>
2020-09-18 13:15:40 -0500

    Transition DIV automatically added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-console.log('hello');
-
-const transitionDIV = document.querySelector('.trans');
-
+const addTransDiv = () => {
+	var div = document.createElement('div');
+	div.classList.add('trans');
+	return document.body.appendChild(div);
+};
 const links = document.querySelectorAll('.panel-item');
 
+const transitionDIV = addTransDiv();
 var func;
-transitionDIV.addEventListener('transitionend', (event) => {
+transitionDIV.addEventListener('transitionend', () => {
 	if (func) {
 		func();
 	}
