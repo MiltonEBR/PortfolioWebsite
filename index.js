02d19afd06a9e66818491c3af847ee0ab9ac65ce
Milton <A01634505@itesm.mx>
2020-09-18 12:57:50 -0500

    First approach at transitions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('hello');
+
+const transitionDIV = document.querySelector('.trans');
+
+const links = document.querySelectorAll('.panel-item');
+
+var func;
+transitionDIV.addEventListener('transitionend', (event) => {
+	if (func) {
+		func();
+	}
+});
+
+for (link of links) {
+	const href = link.href;
+	link.onclick = (event) => {
+		event.preventDefault();
+		console.log(href);
+		func = () => {
+			window.location.href = href;
+		};
+		transitionDIV.classList.add('trans-on');
+	};
+}
