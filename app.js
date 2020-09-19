59b1fb58f384e4fd052d3fb6806e7ec12d34bd53
Milton <A01634505@itesm.mx>
2020-09-19 12:35:18 -0500

    Cards open and close (Triggering multiple times, need to fix)

--- app.js
+++ app.js
@@ -37,16 +37,38 @@ barba.init({
 const buttons = document.querySelectorAll('.icon-btn');
 const icons = document.querySelectorAll('i');
 
+const closeCards = () => {
+	const clicked = document.getElementsByClassName('clicked');
+
+	for (click of clicked) {
+		click.classList.remove('clicked');
+	}
+};
+
+const openCard = (btn) => {
+	if (!btn.classList.contains('clicked')) {
+		btn.classList.add('clicked');
+		btn.onclick = null;
+	}
+};
+
+document.addEventListener('click', (evt) => {
+	var clickInside = false;
+	for (button of buttons) {
+		if (button.contains(evt.target)) {
+			clickInside = true;
+		}
+	}
+
+	if (!clickInside) {
+		closeCards();
+	}
+});
+
 if (buttons.length > 0) {
 	for (button of buttons) {
-		button.onclick = function() {
-			if (!this.classList.contains('clicked')) {
-				this.classList.add('clicked');
-				this.onclick = null;
-			}
-		};
-
 		button.addEventListener('transitionend', function() {
+			console.log('hello');
 			if (!this.classList.contains('clicked')) {
 				this.onclick = function() {
 					if (!this.classList.contains('clicked')) {
@@ -60,11 +82,7 @@ if (buttons.length > 0) {
 
 	for (icon of icons) {
 		icon.onclick = function() {
-			const clicked = document.getElementsByClassName('clicked');
-
-			for (click of clicked) {
-				click.classList.remove('clicked');
-			}
+			closeCards();
 		};
 	}
 }
