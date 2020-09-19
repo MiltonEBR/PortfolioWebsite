148c535afdb5fc709e09ace34e838a7f8cf7565d
Milton <A01634505@itesm.mx>
2020-09-19 12:14:12 -0500

    Card opens and closes

--- app.js
+++ app.js
@@ -35,12 +35,36 @@ barba.init({
 
 //Skills
 const buttons = document.querySelectorAll('.icon-btn');
+const icons = document.querySelectorAll('i');
 
 if (buttons.length > 0) {
 	for (button of buttons) {
 		button.onclick = function() {
-			console.log('hello');
-			this.classList.add('clicked');
+			if (!this.classList.contains('clicked')) {
+				this.classList.add('clicked');
+				this.onclick = null;
+			}
+		};
+
+		button.addEventListener('transitionend', function() {
+			if (!this.classList.contains('clicked')) {
+				this.onclick = function() {
+					if (!this.classList.contains('clicked')) {
+						this.classList.add('clicked');
+						this.onclick = null;
+					}
+				};
+			}
+		});
+	}
+
+	for (icon of icons) {
+		icon.onclick = function() {
+			const clicked = document.getElementsByClassName('clicked');
+
+			for (click of clicked) {
+				click.classList.remove('clicked');
+			}
 		};
 	}
 }
