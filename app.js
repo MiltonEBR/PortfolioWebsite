17c6b60614976fe5b8e918934eef6b3988fd3efc
Milton <A01634505@itesm.mx>
2020-09-20 12:17:36 -0500

    Moved skills into own class

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const transition = () => {
 barba.init({
 	transitions: [
 		{
-			name: 'default-transition',
+			name: 'default',
 			async leave(data) {
 				console.log('hello');
 				transition();
@@ -34,54 +34,5 @@ barba.init({
 	]
 });
 
-//Skills
-
-const buttons = document.querySelectorAll('.icon-btn');
-const icons = document.querySelectorAll('i');
-
-const closeCards = () => {
-	const clicked = document.getElementsByClassName('clicked');
-
-	for (click of clicked) {
-		click.classList.remove('clicked');
-	}
-};
-
-const openCard = function(evt) {
-	var close = false;
-	for (icon of icons) {
-		if (evt.target === icon) {
-			close = true;
-			break;
-		}
-	}
-	if (!this.classList.contains('clicked') && !close) {
-		closeCards();
-		this.classList.add('clicked');
-	}
-};
-
-document.addEventListener('click', (evt) => {
-	var clickInside = false;
-	for (button of buttons) {
-		if (button.contains(evt.target)) {
-			clickInside = true;
-		}
-	}
-
-	if (!clickInside) {
-		closeCards();
-	}
-});
-
-if (buttons.length > 0) {
-	for (button of buttons) {
-		button.onclick = openCard;
-	}
-
-	for (icon of icons) {
-		icon.onclick = function() {
-			closeCards();
-		};
-	}
-}
+const s = new Skills();
+s.run();
