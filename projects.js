6406e3ce59b61abf56f8cc4a66f7693dbbbf7adf
Milton <A01634505@itesm.mx>
2020-09-25 12:34:33 -0500

    Manager of the project expansion logic

---
+++ projects.js
@@ -0,0 +1,47 @@
+class Projects {
+	constructor() {}
+
+	setHidden(panel) {
+		if (panel.classList.contains('show')) {
+			panel.classList.remove('show');
+		}
+
+		if (!panel.classList.contains('hide')) {
+			panel.classList.add('hide');
+		}
+	}
+
+	setVisible(panel) {
+		if (!panel.classList.contains('show')) {
+			panel.classList.add('show');
+		}
+
+		if (panel.classList.contains('hide')) {
+			panel.classList.remove('hide');
+		}
+	}
+
+	run() {
+		const panel = document.querySelector('#proj-panel');
+		// const card = document.querySelector('#full-proj');
+		// panel.addEventListener('click', (evt) => {
+		// 	if (evt.target.contains(card)) {
+		// 		this.setHidden(panel);
+		// 	}
+		// });
+		const closeBtn = panel.querySelector('i');
+		closeBtn.onclick = () => {
+			this.setHidden(panel);
+		};
+		this.setHidden(panel);
+		const cards = document.querySelectorAll('.proj');
+		for (let card of cards) {
+			card.onclick = () => {
+				this.setVisible(panel);
+			};
+		}
+	}
+}
+
+const p = new Projects();
+p.run();
