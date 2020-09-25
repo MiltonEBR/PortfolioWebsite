5442a34c09bbf991f54ffff12e77ddcafee8b641
Milton <A01634505@itesm.mx>
2020-09-25 15:53:18 -0500

    No longer a class

--- projects.js
+++ projects.js
@@ -1,47 +1,42 @@
-class Projects {
-	constructor() {}
-
-	setHidden(panel) {
-		if (panel.classList.contains('show')) {
-			panel.classList.remove('show');
-		}
-
-		if (!panel.classList.contains('hide')) {
-			panel.classList.add('hide');
-		}
+function setHidden(panel) {
+	if (panel.classList.contains('show')) {
+		panel.classList.remove('show');
 	}
 
-	setVisible(panel) {
-		if (!panel.classList.contains('show')) {
-			panel.classList.add('show');
-		}
-
-		if (panel.classList.contains('hide')) {
-			panel.classList.remove('hide');
-		}
-	}
-
-	run() {
-		const panel = document.querySelector('#proj-panel');
-		// const card = document.querySelector('#full-proj');
-		// panel.addEventListener('click', (evt) => {
-		// 	if (evt.target.contains(card)) {
-		// 		this.setHidden(panel);
-		// 	}
-		// });
-		const closeBtn = panel.querySelector('i');
-		closeBtn.onclick = () => {
-			this.setHidden(panel);
-		};
-		this.setHidden(panel);
-		const cards = document.querySelectorAll('.proj');
-		for (let card of cards) {
-			card.onclick = () => {
-				this.setVisible(panel);
-			};
-		}
+	if (!panel.classList.contains('hide')) {
+		panel.classList.add('hide');
 	}
 }
 
-const p = new Projects();
-p.run();
+function setVisible(panel) {
+	if (!panel.classList.contains('show')) {
+		panel.classList.add('show');
+	}
+
+	if (panel.classList.contains('hide')) {
+		panel.classList.remove('hide');
+	}
+}
+
+function run() {
+	const panel = document.querySelector('#proj-panel');
+	// const card = document.querySelector('#full-proj');
+	// panel.addEventListener('click', (evt) => {
+	// 	if (evt.target.contains(card)) {
+	// 		this.setHidden(panel);
+	// 	}
+	// });
+	const closeBtn = panel.querySelector('i');
+	closeBtn.onclick = () => {
+		this.setHidden(panel);
+	};
+	this.setHidden(panel);
+	const cards = document.querySelectorAll('.proj');
+	for (let card of cards) {
+		card.onclick = () => {
+			this.setVisible(panel);
+		};
+	}
+}
+
+run();
