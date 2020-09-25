28a205276f08c2f0b54ac7803a92df76dabdb535
Milton <A01634505@itesm.mx>
2020-09-25 15:48:30 -0500

    No longer a class and reduced the code

--- skills.js
+++ skills.js
@@ -1,63 +1,56 @@
-class Skills {
-	constructor() {
-		this.buttons = null;
-		this.icons = null;
+function closeCards() {
+	const clicked = document.getElementsByClassName('clicked');
+
+	for (let click of clicked) {
+		click.classList.remove('clicked');
 	}
+}
 
-	closeCards() {
-		const clicked = document.getElementsByClassName('clicked');
-
-		for (let click of clicked) {
-			click.classList.remove('clicked');
-		}
-	}
-
-	openCard(icons) {
-		const closeCards = this.closeCards;
-		if (icons)
-			return function(evt) {
-				var close = false;
-				for (let icon of icons) {
-					if (evt.target === icon) {
-						close = true;
-						break;
-					}
-				}
-				if (!this.classList.contains('clicked') && !close) {
-					closeCards();
-					this.classList.add('clicked');
-				}
-			};
-	}
-
-	run() {
-		this.buttons = document.querySelectorAll('.icon-btn');
-		this.icons = document.querySelectorAll('i');
-
-		const closeCards = this.closeCards;
-		document.addEventListener('click', (evt) => {
-			var clickInside = false;
-			for (let button of this.buttons) {
-				if (button.contains(evt.target)) {
-					clickInside = true;
+function openCard(icons) {
+	if (icons)
+		return function(evt) {
+			var close = false;
+			for (let icon of icons) {
+				if (evt.target === icon) {
+					close = true;
+					break;
 				}
 			}
-
-			if (!clickInside) {
+			if (!this.classList.contains('clicked') && !close) {
 				closeCards();
+				this.classList.add('clicked');
 			}
-		});
+		};
+}
 
-		if (this.buttons.length > 0) {
-			for (let button of this.buttons) {
-				button.onclick = this.openCard(this.icons);
-			}
+function run() {
+	buttons = document.querySelectorAll('.icon-btn');
+	icons = document.querySelectorAll('i');
 
-			for (let icon of this.icons) {
-				icon.onclick = function() {
-					closeCards();
-				};
+	document.addEventListener('click', (evt) => {
+		var clickInside = false;
+		for (let button of buttons) {
+			if (button.contains(evt.target)) {
+				clickInside = true;
 			}
+		}
+
+		if (!clickInside) {
+			closeCards();
+		}
+	});
+
+	if (buttons.length > 0) {
+		for (let button of buttons) {
+			button.onclick = openCard(icons);
+		}
+
+		for (let icon of icons) {
+			icon.onclick = function() {
+				closeCards();
+			};
 		}
 	}
 }
+
+run();
